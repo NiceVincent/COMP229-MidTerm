522b9e921b888e68f524b2e9485f770cb66ed3fd
v <>
2023-03-01 12:06:13 -0500

    chore: set config url

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
-  // 
+  // "URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://books229:BvUMDKWBSSJlyJGI@cluster0.24ohmya.mongodb.net/?retryWrites=true&w=majority"
 };
